5f49e0ea485e44dba2c5e33fc01a7a54db41b5fd
jeremysoh0210 <jeremysoh222@gmail.com>
2023-12-16 15:59:08 +0000

    Create a CNN model for CIFAR-10 classification :pencil:

--- preprocessing.py
+++ preprocessing.py
@@ -152,3 +152,15 @@ X_test = X_test.reshape(-1, 32, 32, 3)
 y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
 y_val = tf.keras.utils.to_categorical(y_val, num_classes=10)
 y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+# Build a CNN model using activation function relu
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+

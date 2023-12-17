2c5c7682e2d6350fd5cf928fcb7abcb3a826b261
jeremysoh0210 <jeremysoh222@gmail.com>
2023-12-17 17:29:39 +0000

    Create a new file with different read data(previous code not working on google colaboratory)

---
+++ data_preprocessing.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.model_selection import train_test_split
+import tensorflow.keras
+import pickle
+import random
+import keras
+import requests
+import tensorflow as tf
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from keras.layers import Conv2D, MaxPooling2D
+from PIL import Image
+from keras import models, layers
+from keras.datasets import cifar10, cifar100
+
+# Load data from CIFAR10 and CIFAR100
+(cifar10_x_train, cifar10_y_train), (cifar10_x_test, cifar10_y_test) = cifar10.load_data()
+(cifar100_x_train, cifar100_y_train), (cifar100_x_test, cifar100_y_test) = cifar100.load_data()
+
+# filter out classes from CIFAR10 and CIFAR100
+# 1, 2, 3, 4, 5, 7, 9                                                               = automobile, bird, cat, deer, dog, horse, and truck
+# 2, 8, 11, 13, 19, 34, 35, 41, 46, 47, 48, 52, 56, 58, 59, 65, 80, 89, 90, 96, 98  = cattle, fox, baby, boy, girl, man, woman, rabbit, squirrel, trees(superclass), bicycle, bus, motorcycle, pickup truck, train, lawn-mower and tractor
+cifar10_classes = [1, 2, 3, 4, 5, 7, 9]
+cifar100_classes = [2, 8, 11, 13, 19, 34, 35, 41, 46, 47, 48, 52, 56, 58, 59, 65, 80, 89, 90, 96, 98]
+
